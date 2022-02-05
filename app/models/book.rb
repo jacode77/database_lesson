66b580564f8707d49fb1d2a3151f59e32ce9cbc2
Jacqui <jt.cope1@gmail.com>
2022-02-05 08:00:21 +0800

    adds table connections

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
     # validates :genre, presence: true
-    validates_presence_of :title, :author, :genre
+    validates_presence_of :title, :author
     belongs_to :author
     has_many :books_genres
     has_many :genres, through: :books_genres
